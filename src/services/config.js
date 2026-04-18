4b55c28b5722fed08b49a656b04fb2c0fd029701
manaswini1610 <manaswini161009@gmail.com>
2026-04-18 18:23:03 +0530

    deployed

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,5 @@
-const BaseUrl = "http://localhost:5000/api";
+// const BaseUrl = "http://localhost:5000/api";
+const BaseUrl = "https://lic-app-backend.vercel.app/api";
 
 const API_URLS = {
   Policies: `${BaseUrl}`,
